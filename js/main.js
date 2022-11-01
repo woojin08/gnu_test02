9ca14733c95eaf9b7a35f48e1aadbe2397922372
woojin08 <105770875+woojin08@users.noreply.github.com>
2022-11-01 15:37:59 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function () {
-    $('.main_slide').slick()
+    $('.main_slide').slick({
+        autoplay: true,
+        autoplaySpeed: 4500,
+        arrows: false,
+        dots: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    })
 })
